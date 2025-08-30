1c4d1f2ed503289f5fd2b8d51d185bfad1265866
Msun-ny <suhyun224@gmail.com>
2025-08-30 13:20:57 +0900

    chore: 안쓰는 ValidationMessage 주석 처리

--- paekom/src/main/java/com/paekom/global/exception/ValidationMessage.java
+++ paekom/src/main/java/com/paekom/global/exception/ValidationMessage.java
@@ -4,8 +4,8 @@ public class ValidationMessage {
 
     private ValidationMessage() {}
         // User
-    public static final String NICKNAME_NOT_EMPTY = "닉네임은 필수값 입니다.";
-    public static final String ERROR_NICKNAME_LENGTH = "길이를 초과했습니다.";
-    public static final String ERROR_NICKNAME_FORMAT = "이름은 숫자나 특수문자를 포함할 수 없습니다.";
+//    public static final String NICKNAME_NOT_EMPTY = "닉네임은 필수값 입니다.";
+//    public static final String ERROR_NICKNAME_LENGTH = "길이를 초과했습니다.";
+//    public static final String ERROR_NICKNAME_FORMAT = "이름은 숫자나 특수문자를 포함할 수 없습니다.";
 
 }
