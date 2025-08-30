77106a0b14d10814f061a137bc8e8583c5010952
Msun-ny <suhyun224@gmail.com>
2025-08-30 12:10:41 +0900

    feat: 스프링 부트 프로젝트 초기 세팅(JPA/MySQL/OAuth2)

---
+++ paekom/src/main/java/com/paekom/PaekomApplication.java
@@ -0,0 +1,13 @@
+package com.paekom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaekomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaekomApplication.class, args);
+    }
+
+}
