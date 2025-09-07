b51581b116b51d5071ababc5aa30ea0e38a1fe7d
Msun-ny <suhyun224@gmail.com>
2025-09-08 00:17:52 +0900

    feat: WebSocketConfig, SignalingHandler, RoomManager 추가

---
+++ paekom/src/main/java/com/paekom/global/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.paekom.global.config;
+
+import com.paekom.websocket.SignalingHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final SignalingHandler signalingHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        // origin 전체 허용(로컬 개발)
+        registry.addHandler(signalingHandler, "ws/signaling")
+                .setAllowedOriginPatterns("*");
+    }
+}
